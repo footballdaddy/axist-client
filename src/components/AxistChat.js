339d3c2d1b8e28e51d5d14a837b7d917d210d0bf
Kanya Paramita <kanyaparamita@yahoo.com>
2017-07-23 17:07:04 +0700

    Add socket, need to work on displaying messages

--- src/components/AxistChat.js
+++ src/components/AxistChat.js
@@ -1,32 +1,106 @@
 import React, { Component } from 'react';
-import ChatBot from 'react-simple-chatbot';
+import ChatBot from '../components/chatbot/ChatBot';
+import _ from 'underscore';
+import moment from 'moment';
 
 const io = require('socket.io-client')
-const socket = io()
+const socket = io.connect('http://139.59.233.76:8080')
 
 class AxistChat extends Component {
-  render() {
-    const steps=[
-      {
-        id: '1',
-        message: 'What is your name?',
-        trigger: '2',
-      },
-      {
-        id: '2',
-        user: true,
-        trigger: '3',
-      },
-      {
-        id: '3',
-        message: 'Hi {previousValue}, nice to meet you!',
-        trigger: '2',
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      steps : [
+        {
+          id: 'init',
+          message: 'Hi, I am Axist your personal assistive chatbot. What can I do for you?',
+          trigger: 'answer'
+        },
+        {
+          id: 'answer',
+          user: true,
+          validator: (value) => this.sendAnswer(value),
+          trigger: 'answer'
+        }
+      ],
+      last_step_id: 'init',
+      user : {},
+      is_anonymous : true
+    };
+
+    socket.on('conversation', (receivedMessage) => this.receiveMessage(receivedMessage));
+  }
+
+  componentDidUpdate() {
+    // socket.on('conversation', (receivedMessage) => this.receiveMessage(receivedMessage));
+  }
+
+  updateSteps(newStep) {
+    var self = this;
+    var steps = this.state.steps;
+
+    steps.push(newStep);
+
+    var updatedSteps = _.map(
+      steps,
+      function(step) {
+        console.log('old last step id ', self.state.last_step_id);
+
+        if (step.id === self.state.last_step_id) {
+          return _.extend(step, {
+            trigger: newStep.id
+          });
+        } else {
+          return step;
+        }
       }
-    ];
+    );
+
+    this.setState({
+      steps: updatedSteps,
+      last_step_id: newStep.id
+    });
+  }
+
+  sendAnswer(message) {
+    console.log('sendAnswer ', message);
+
+    socket.emit('conversation', {
+      user_id: 'kanya',
+      message: message
+    });
+
+    var newStep = {
+      id: moment().format('DDMMYYYYHHmmssSSS'),
+      user: true,
+      message: message,
+      trigger: 'answer'
+    };
+
+    this.updateSteps(newStep);
+
+    return true;
+  }
+
+  receiveMessage(receivedMessage) {
+    console.log('receivedMessage ', receivedMessage);
+
+    var newStep = {
+      id: receivedMessage.message.id,
+      message: receivedMessage.message.response,
+      trigger: 'answer'
+    };
+
+    this.updateSteps(newStep);
+  }
+
+  render() {
+    console.log('state steps ', this.state.steps);
 
     return (
       <div>
-        <ChatBot floating steps={steps} />
+        <ChatBot floating steps={ this.state.steps }/>
       </div>
     );
   }
---
+++ src/components/chatbot/schemas/userSchema.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    key: 'id',
+    types: ['string', 'number'],
+    required: true,
+  },
+  {
+    key: 'user',
+    types: ['boolean'],
+    required: true,
+  },
+  {
+    key: 'message',
+    types: ['string'],
+    required: false,
+  },
+  {
+    key: 'trigger',
+    types: ['string', 'number'],
+    required: false,
+  },
+  {
+    key: 'validator',
+    types: ['function'],
+    required: false,
+  },
+  {
+    key: 'end',
+    types: ['boolean'],
+    required: false,
+  },
+];
